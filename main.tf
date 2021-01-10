73dfe2bde0a1374524115198a191be6e6f3d7ff7
VINAMRA MITTAL <vinamra.mittal355@gmail.com>
2021-01-10 22:42:19 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,28 +1,13 @@
 
-provider "aws" {
-  region = "eu-central-1"
-}
-
-## Networking
-module "networking" {
-  source = "./modules/networking/"
+## Wordpress
+module "wordpress" {
+  source = "./wordpress"
 
   ## Networking Service
   networking_module = var.networking_module
 
-  ## Meta
-  meta = var.meta
-}
-
-## APPS
-module "apps" {
-  source = "./apps"
-
-  ## Networking Service
-  networking_module = module.networking
-
-  ## Apps
-  apps = var.apps
+  ## Application Config
+  app_config = var.apps.wordpress
 
   ## Meta
   meta = var.meta
